5e9fa4013bd7ebcf6946aa80cd2155802613e153
BlueCodeThree <carliemhamilton@gmail.com>
2020-03-02 21:22:30 +1000

    adding more stuff to my app

--- app.ts
+++ app.ts
@@ -1,13 +1,32 @@
 const express = require( "express" );
+const cors = require("cors");
 const app = express();
-const port = 8080; // default port to listen
 
-// define a route handler for the default home page
+// Routes
 app.get( "/", ( req:any, res:any ) => {
     res.send( "Hello world!" );
 } );
 
+// Environment Variables
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+const port = process.env.PORT || 3003;
+
 // start the Express server
+app.use(cors());
+app.use(express.json())
+// connect to the database, process.env.NODE_ENV
+
+// Defining the routes
+
+// Listen to server
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
+    console.log( `BiblioFile - server listening at http://localhost:${ port }` );
 } );
+
+// export app
+module.exports = {
+    app
+}
