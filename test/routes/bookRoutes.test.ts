3b5b2bbfb61e2e42a70867f1c1360883ba7c77f3
BlueCodeThree <carliemhamilton@gmail.com>
2020-03-08 19:05:08 +1000

    tests are too much trouble. Blah. Will try again with tests later

---
+++ test/routes/bookRoutes.test.ts
@@ -0,0 +1,34 @@
+// import chai from 'chai';
+// import chaiHttp = require('chai-http');
+// chai.use(chaiHttp);
+// const { expect } = chai;
+// const should = chai.should();
+// import { app } from "../../app";
+
+// describe("Book Routes Test - Google Books API", () => {
+
+//     // GET /books/
+//     // This is for searching for a book on the google API
+//     describe('GET books/', () => {
+//         it('should find an array of books from the google api', (done) => {
+//             // chai.res(app)
+//             //     .get('/books')
+//             //     .send()
+//             done();
+//         })
+
+//         it('should return "nothing returned" if there are no results', done => {
+//             done();
+//         })
+//     })
+
+//     describe('GET book/:id', () => {
+//         it('should find a book from the google api if given a valid id', done => {
+//             done();
+//         })
+
+//         it('should return an error not found if the id was invalid or nothing was found', done => {
+//             done();
+//         })
+//     })
+// })
