1069a740b8047c78159890babcc48dd7de651548
BlueCodeThree <carliemhamilton@gmail.com>
2020-03-08 18:18:35 +1000

    oh yeah, finally got this working and also I learnt some things

--- controllers/bookController.ts
+++ controllers/bookController.ts
@@ -12,7 +12,7 @@ export const getBooks = async (req: express.Request, res: express.Response) => {
             res.status(200).send(searchResponse);
         })
         .catch(err => {
-            res.status(500).send(err)
+            res.status(500).send(err);
 
         })
 }
@@ -22,16 +22,14 @@ export const getBook =  async (req: Request, res: Response) => {
     const { id } = req.params;
     console.log(id);
     console.log("get on '/book/:id")
-    // axios.get(`https://www.googleapis.com/books/v1/volumes/${id}?key=${process.env.GOOGLE_API_KEY}`)
-    axios.get(`https://www.googleapis.com/books/v1/volumes/QVn-CgAAQBAJ?key=${process.env.GOOGLE_API_KEY}`)
-        .then(res => {
-            // handle success
-            console.log(res.data);
-            return res.send(res.data);
+    axios.get<ServerResponse>(`https://www.googleapis.com/books/v1/volumes/${id}?key=${process.env.GOOGLE_API_KEY}`)
+    // axios.get(`https://www.googleapis.com/books/v1/volumes/QVn-CgAAQBAJ?key=${process.env.GOOGLE_API_KEY}`)
+        .then(response => {
+            const bookData = response.data;
+            res.send(bookData);
         })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
+        .catch(function (err) {
+            res.status(500).send(err);
         })
 }
 
