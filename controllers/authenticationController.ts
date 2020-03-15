4d8d09761f8c053753db9cdab4a41706f2b0ee90
BlueCodeThree <carliemhamilton@gmail.com>
2020-03-15 23:57:07 +1000

    adds some auth methods but haven't tested yet, I'm tired

--- controllers/authenticationController.ts
+++ controllers/authenticationController.ts
@@ -1,8 +1,42 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import express from 'express';
+import { User } from '../models/user';
 
-// const login = (req: Express.Request, res: Express.Response) => {
-//     let username = req.body.username;
-//     let
-// }
+export const register = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const userData = req.body;
+
+    if (await User
+                .findOne({
+                    email: userData.email
+                })) {
+        // next(new UserWithThatEmailAlreadyExistsException(userData.email));
+    } else {
+        const hashedPassword = await bcrypt.hash(userData.password, 10);
+        const user = await User.create({
+            ...userData,
+            password: hashedPassword,
+        });
+        user.password = undefined;
+        res.send(user);
+    }
+}
+
+export const login = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const logInData = req.body;
+    const user = await User.findOne({
+        email: logInData.email
+    });
+
+    if (user) {
+        const isPasswordMatching = await bcrypt.compare(logInData.password, user.password);
+        if (isPasswordMatching) {
+            user.password = undefined;
+            res.send(user);
+        } else {
+            // next(new WrongCredentialsException())
+        }
+    } else {
+        // next(new WrongCredentialsException())
+    }
+}
--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,15 @@
-export interface User {
-    username: string;
-    email: string;
-    password: string;
-}
+import mongoose from 'mongoose';
+
+interface User {
+    username: string,
+    email: string,
+    password: string
+};
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String
+});
+
+export const User = mongoose.model<User & mongoose.Document>('User', userSchema);
