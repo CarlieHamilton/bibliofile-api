9289b708973c3844f637ca166cfb54adfb68a867
BlueCodeThree <carliemhamilton@gmail.com>
2020-10-16 19:46:36 +1000

    I'm making such good progress! Who would have thought that saving something to the wishlist would be so hard. Maybe I have overcomplicated this. I have overcomplicated haven't I....

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,5 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import { ServerResponse } from 'http';
 import bookModel from './book.model';
+import { BookId, Book } from './book.interface';
 
-export const retrieveBookById = (id) => {
+export const retrieveBookById = (id: BookId) => {
     return bookModel.findById(id);
+}
+
+export const retrieveBookByGoogleId = async (id: BookId) => {
+    const book = await bookModel.findOne({googleInfo: {id: id}}).exec();
+    return book;
+}
+
+export const retrieveBookFromGoogleById = async (id) => {
+    try {
+        const {data:response} = await axios.get<ServerResponse>(`https://www.googleapis.com/books/v1/volumes/${id}?key=${process.env.GOOGLE_API_KEY}`)
+        return response
+      }
+
+      catch (error) {
+        console.log(error);
+      }
+};
+
+export const convertGoogleBookToBook = (googleBookData) => {
+
+}
+
+export const saveBookToDatabase = async (bookData) => {
+    const book = new bookModel(bookData);
+    book.save()
+        .then(book => {
+            console.log("saved")
+        })
 }
--- src/wishlist/wishlist.controller.ts
+++ src/wishlist/wishlist.controller.ts
@@ -1,33 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
-import axios from 'axios';
-import { ServerResponse } from 'http';
-import wishlistModel from './wishlist.model';
-import { BookId } from '../books/book.interface';
-import bookModel from '../books/book.model';
-import { getBookFromGoogle } from 'books/books.controller';
+// import axios from 'axios';
+// import { ServerResponse } from 'http';
+// import wishlistModel from './wishlist.model';
+// import { BookId } from '../books/book.interface';
+// import bookModel from '../books/book.model';
+// import { getBookFromGoogle } from 'books/books.controller';
+import { retrieveBookByGoogleId, retrieveBookFromGoogleById, saveBookToDatabase } from '../books/books.service';
 
 // POST wishlist to database
 export const addBookToWishlist = async (request: Request, response: Response) => {
     const { bookId: googleBookId } = request.body;
-    // console.log(bookId);
-    // check if the book is already in our database
-    bookModel.findOne({googleInfo: {id: googleBookId }},  (err, response) => {
-        if (err){
-        //        axios.get<ServerResponse>(`https://www.googleapis.com/books/v1/volumes/${bookId}?key=${process.env.GOOGLE_API_KEY}`)
-        // .then(res => {
-        //     const bookData = res.data;
-        //     response.send(bookData);
-        // })
-        // .catch(err => {
-        //     response.status(500).send(err);
-        // })
-            // get the book from google
-            // add the book to the database
-        }
-        else{
-            console.log("Result : ", response);
-        }
-    });
+    let book = await retrieveBookByGoogleId(googleBookId);
+
+    if (book == null) {
+        const bookFromGoogle = await retrieveBookFromGoogleById(googleBookId);
+        console.log(bookFromGoogle)
+        // add the book to the database
+        saveBookToDatabase(bookFromGoogle);
+    }
+    console.log(book)
     // need the current logged in user.
     // if it isn't, add it to the database --> use POST in books.controller
     // have book id
